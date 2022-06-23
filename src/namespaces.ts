9275619ef36f5451ed3e59af1c95b5fde52f8f26
Carlo Segat <segatcarlo95@gmail.com>
2022-06-23 11:02:15 +0200

    added namespaces

--- src/namespaces.ts
+++ src/namespaces.ts
@@ -2,5 +2,7 @@ import namespace from '@rdfjs/namespace'
 export const ns = {
   sh: namespace('http://www.w3.org/ns/shacl#'),
   cfrl: namespace('http://www.cefriel.com/shacl-forms#'),
-  dash: namespace('http://datashapes.org/dash#')
+  dash: namespace('http://datashapes.org/dash#'),
+  dcat: namespace('http://www.w3.org/ns/dcat#'),
+  rdf: namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
 }
